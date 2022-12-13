cfaa88a9d39094f28d2ef7076539a2e0e43e9b2a
Jesus Ramirez <jesusramirez730@gmail.com>
2022-12-13 01:28:53 -0800

    testing file

---
+++ test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        boolean isAlive = false;
+
+        if(isAlive){
+            System.out.println("is alive");
+        } else {
+            System.out.println("is dead");
+        }
+    }
+}
